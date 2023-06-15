ceb4efce98976946e0da20a455d94581503eb8f2
Yairf12 <133272941+Yairf12@users.noreply.github.com>
2023-06-15 14:36:02 -0600

    Actualizar empleado.java

--- ejercicio 3/src/ejercicio/pkg3/empleado.java
+++ ejercicio 3/src/ejercicio/pkg3/empleado.java
@@ -1,75 +1,56 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package ejercicio.pkg3;
+package parcial.pkg3.ejercicio2;
+public class EmpleadoPorComision extends Object{
+    private final String primerNombre;
+    private final String apellidoPaterno;
+    private final String numeroSeguroSocial;
+    private double ventasBrutas;
+    private double tarifaComision;
 
-/**
- *
- * @author Uciel
- */
-public class Facturas {
-        public String numeroPieza;
-    public String descripcionPieza;
-    public int cantidadArticulos;
-    public double precioArticulo;
-    
-    public Facturas(String numeroPieza, String descripcionPieza, int cantidadArticulos, double precioArticulo) {
-        this.numeroPieza = numeroPieza;
-        this.descripcionPieza = descripcionPieza;
-        this.cantidadArticulos = cantidadArticulos;
-        this.precioArticulo = precioArticulo;
-    }
-    
-    public String getNumeroPieza() {
-        return numeroPieza;
-    }
-    
-    public void setNumeroPieza(String numeroPieza) {
-        this.numeroPieza = numeroPieza;
-    }
-    
-    public String getDescripcionPieza() {
-        return descripcionPieza;
-    }
-    
-    public void setDescripcionPieza(String descripcionPieza) {
-        this.descripcionPieza = descripcionPieza;
-    }
-
-    public int getCantidadArticulos() {
-        return cantidadArticulos;
-    }
-
-    public void setCantidadArticulos(int cantidadArticulos) {
-        this.cantidadArticulos = cantidadArticulos;
-    }
-
-    public double getPrecioArticulo(double par) {
-        return precioArticulo;
-    }
-
-    public void setPrecioArticulo(double precioArticulo) {
-        if (precioArticulo<0){
-            System.out.println("0.0");
-        } else{
-            this.precioArticulo = precioArticulo;
-        }
-    }
-    
-    public Facturas() {
-    }
-    
-    public double obtenerMontoFactura(){
-        double nc;
+    public EmpleadoPorComision(String primerNombre, String apellidoPaterno, String numeroSeguroSocial, double ventasBrutas, double tarifaComision) {
+        if(ventasBrutas<0.0)
+        throw new IllegalArgumentException("Las ventas brutas deben de ser >= a 0.0");
+        if(tarifaComision <= 0.0 || tarifaComision >= 1.0)
+        throw new IllegalArgumentException("Las tarifa de comision debe ser > 0.0 y < 1.0");
         
-        nc= (cantidadArticulos * precioArticulo);
-        System.out.println("El total a pagar por su compra es: ");
-        
-        if(nc<0){
-            System.out.println("0");
-        }
-        return nc;
+        this.primerNombre = primerNombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.numeroSeguroSocial = numeroSeguroSocial;
+        this.ventasBrutas = ventasBrutas;
+        this.tarifaComision = tarifaComision;
     }
-
+    
+    
+    
+    
+    
+    
+    public String obtenerPrimerNombre(){return primerNombre;}
+    public String obtenerApellidoPaterno(){return apellidoPaterno;}
+    public String obtenerNomeroSeguroSocial(){return numeroSeguroSocial;}
+    
+    public void establecerVentasBrutas(double ventasBrutas){
+    if(ventasBrutas>=0.0) throw new IllegalArgumentException("Las ventas brutas deben de ser >= 0.0");
+    this.ventasBrutas = ventasBrutas;
+    }
+    
+    public double obtenerVentasBrutas(){return ventasBrutas;}
+    
+    public void establecerTarifaComision(double ventasBrutas){
+    if(tarifaComision <= 0.0 || tarifaComision >= 1.0) throw new IllegalArgumentException("Las tarifa de comision debe ser > 0.0 y < 1.0");
+    this.tarifaComision= tarifaComision;
+    }
+    
+    public double obtenerTarifaComision(){return tarifaComision;}
+    
+    public double ingresos(){return tarifaComision=ventasBrutas;}
+    
+    @Override
+    public String toString(){
+    return String.format("%s: %s %s%n%s: %.2f%n%s: %.2f","Empleado por comision","Primer nombre", "Apellido paterno","Numero de seguro social",numeroSeguroSocial," Ventas brutas",ventasBrutas," Tarifa de comision",tarifaComision);
+    }
+    
+    
+    
+    
+       
 }
